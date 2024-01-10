cd0a41ff0902c4c4add676f62e7d36ea8929f827
SirromEloc <aidan.cole.morris@gmail.com>
2024-01-10 10:57:43 -0600

    compiles; program currently converts input to enum and removes spaces

--- main.swift
+++ main.swift
@@ -1,5 +1,6 @@
 //defines data type used to break apart the equation
 indirect enum ParseUnit {
+    case unparsed(String)
     case operate(op: Operating, leftBranch: ParseUnit, rightBranch: ParseUnit)
     case variable(v: String)
 }
@@ -16,17 +17,20 @@ enum Operating {
 }
 
 //start of program
-func main() {
-    print("Enter your Boolean Equation")
-    if var equationString = readLine() {  //receives user equation
-        equationString.replace(" ", with: "") //removes all spaces
+print("Enter your Boolean Equation")
+if var equationString = readLine() {  //receives user equation
+    equationString.replace(" ", with: "") //removes all spaces
     
-        var parsedArray = parse(input: equationString)
-    } else { //if nothing is inputted, just tells them that
-        print("Did not enter an equation")
+    var equation : ParseUnit = ParseUnit.unparsed(equationString)
+    parse(&equation)
+    
+        
+} else { //if nothing is inputted, just tells them that
+    print("Did not enter an equation")
 }
 
-func parse(_ input: String) -> Array {
-    
+
+func parse(_ input:inout ParseUnit) {
+    print(input)
 }
 
