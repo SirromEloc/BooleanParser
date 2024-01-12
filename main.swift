04659d7c21704f8eb462d8bb05db07469abae449
SirromEloc <aidan.cole.morris@gmail.com>
2024-01-12 08:48:28 -0600

    Started tokenizing, really lost tho

--- main.swift
+++ main.swift
@@ -1,12 +1,12 @@
 //defines data type used to break apart the equation
 indirect enum ParseUnit {
-    case unparsed(String)
     case operate(op: Operating, leftBranch: ParseUnit, rightBranch: ParseUnit)
     case variable(v: String)
 }
 
 enum Operating {
-    case parenthesis
+    case rpar
+    case lpar
     case negation
     case land
     case lor
@@ -18,11 +18,10 @@ enum Operating {
 
 //start of program
 print("Enter your Boolean Equation")
-if var equationString = readLine() {  //receives user equation
-    equationString.replace(" ", with: "") //removes all spaces
-    
-    var equation : ParseUnit = ParseUnit.unparsed(equationString)
-    parse(&equation)
+if let equationString = readLine() {  //receives user equation
+    //need translating function here
+
+    let tokenized = tokenizer(equationString)
     
         
 } else { //if nothing is inputted, just tells them that
@@ -30,7 +29,25 @@ if var equationString = readLine() {  //receives user equation
 }
 
 
-func parse(_ input:inout ParseUnit) {
-    print(input)
+func tokenizer(_ input: String) -> Array<Any> {
+    let operators : [Character : Operating] = [
+      "^" : .land,
+    ]
+
+    var parseArray : Array<Any> = []
+    
+    
+    for (index, char) in input.enumerated() {
+        while char != " " {
+            if let token  = operators[char] {
+                parseArray.append(token)
+            }
+           
+        }
+    }
+    return parseArray
 }
 
+func makeUnit(op: Operating, leftBranch: String, rightBranch: String) {
+    
+}
