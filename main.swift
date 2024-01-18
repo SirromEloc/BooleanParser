8e2ccfb8690cc993a0e391d155320a533c21a476
SirromEloc <aidan.cole.morris@gmail.com>
2024-01-18 08:29:18 -0600

    Complete Restart based on Teacher Assistance with Regular Expressions

--- main.swift
+++ main.swift
@@ -1,81 +1,36 @@
+import Foundation
 
-//MR BEN HELP - ERROR HANDLING - DO IT IN ENUM
-enum TokenError : Error {
-    case unableToFindToken
-    case cannotDivideByZero
-}
-//do -> catch blocks
-//throw process
-//throw propagates an error down the stack
-//examples
-
-func badFunc(dividend: Int, divisor: Int) throws -> Int {
-    guard { divisor > 0 } else { throw TokenError.cannotDivideByZero }
-    return dividend / divisor
-}
-func example() {
-    do {
-        badFunc(dividend: 4, divisor: 0)
-    } catch {
-        print("Failed Because \(error)")
-    }
-}
-
-
-
-
-
-//defines data type used to break apart the equation
-indirect enum ParseUnit {
-    case operate(op: Operating, leftBranch: ParseUnit, rightBranch: ParseUnit)
-    case variable(v: String)
-}
-
-enum Operating {
-    case rpar
-    case lpar
-    case negation
-    case land
-    case lor
-    case xor
-    case implies
-    case equation
-    case nequation
-}
-
-//start of program
-print("Enter your Boolean Equation")
-if let equationString = readLine() {  //receives user equation
-    //need translating function here
-
-    let tokenized = tokenizer(equationString)
-    
+enum Lexeme {
+    enum LexemeError: Error {
+        case failedToFindLexeme(inLine: String)
         
-} else { //if nothing is inputted, just tells them that
-    print("Did not enter an equation")
-}
+    }
 
+    case variable(name: String)
+    case operators(value: Int)
 
-func tokenizer(_ input: String) -> Array<Any> {
-    let operators : [Character : Operating] = [
-      "^" : .land,
-    ]
-
-    var parseArray : Array<Any> = []
-    var tempVarBuilder : String = ""
-    
-    for (index, char) in input.enumerated() {
-        if char != " " {
-            if let token  = operators[char] {
-                parseArray.append(token)
-            }  else {
-                tempVarBuilder += char
-            }
+    static func nextLexeme(from line: Substring) throws -> [Lexeme] {
+        var Lexemes : [Lexeme] = []
+        if let match = try /\s*[a-zA-Z]*\s/.firstMatch(in: line) {
+            Lexemes.append(Lexeme.variable(name: String(match.0)))
+            //need to get range from match substring
+            return Lexemes + self.nextLexeme(from: Substring)
+        } else if let match = try /\s*=\s/.prefixMatch(in: line) {
+            print("Found assignment operator")
+        } else {
+            throw LexemeError.failedToFindLexeme(inLine: String(line))
         }
     }
-    return parseArray
-}
-
-func makeUnit(op: Operating, leftBranch: String, rightBranch: String) {
     
 }
+
+func main() {
+    do {
+        let lexemes: [Lexeme] = try Lexeme.nextLexeme(from: " var x = 123 var ")
+        print(lexemes)
+    } catch {
+        print("Failed because \(error).")
+    }
+}
+
+main()
