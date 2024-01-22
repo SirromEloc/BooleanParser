527b50127c5793a5545efc28e3653d00389ab5fb
SirromEloc <aidan.cole.morris@gmail.com>
2024-01-22 09:09:56 -0600

    great teacher guidance lol, tuple return and lexemeSource struct

--- main.swift
+++ main.swift
@@ -3,49 +3,57 @@ import Foundation
 enum Lexeme {
     enum LexemeError: Error {
         case failedToFindLexeme(inLine: String)
+        case inconsistentSpacingForOperator(operator: String, inLine: String)
         
     }
 
     case variable(name: String)
     case operators(value: String)
 
-    static func nextLexeme(from line: Substring) throws -> [Lexeme] {
-        var Lexemes : [Lexeme] = []
+    static func nextLexeme(from line: Substring) throws ->
+      (lexeme: Lexeme, line: String, charactersUsed: Int) {
         if let match = try /\s*([a-z]+)\s/.firstMatch(in: line) {
-            
-            //get range from match substring
-            let matchRange = NSRange(match.range, in: line)
-            print(matchRange)
-
-            //removes leading and trailing spaces from match, then adds to array
-            Lexemes.append(.variable(name: String(match.1)))
-
-            /* This would recurrsively run the program until 
-            if (matchRange.upperBound) < (line.count - 1) {
-                let index = line.index(line.endIndex, offsetBy: matchRange.upperBound)
-                let next = try self.nextLexeme(from: line.suffix(from: index))
-                return Lexemes + next
-                
-            } else {
-                return Lexemes
-            } 
-            */
+            return (lexeme: Lexeme.variable(name: String(match.1)),
+                    line: String(match.0),
+                    charactersUsed: match.0.count)
                         
-        } else if let match = try /\s*(\+)\s*/.prefixMatch(in: line) { //only suited for plus sign currently
-            Lexemes.append(.operators(value: String(match.1)))
+        } else if let match = try /(\s*)(\+)(\s*)/.prefixMatch(in: line) { //only suited for plus sign currently
+            guard (match.1.count == 0 && match.3.count == 0) ||
+                    (match.1.count > 0 && match.3.count > 0) else {
+                throw LexemeError.inconsistentSpacingForOperator(operator: "+", inLine: String(line))
+            }
+            
+            return (lexeme: Lexeme.operators(value: String(match.2)),
+                    line: String(match.0),
+                    charactersUsed: match.0.count)
             
         } else {
             throw LexemeError.failedToFindLexeme(inLine: String(line))
         }
-
-        return Lexemes
     }
 }
 
+struct LexemeSource {
+    let lexeme: Lexeme
+    let text: String
+    let lineIndex: Int
+    let columnIndex: Int
+
+    static func lex(line: String) throws {
+        var workingLine = Substring(line)
+        while workingLine.count > 0 {
+            let (lexeme, text, charactersUsed) = try Lexeme.nextLexeme(from: workingLine)
+            workingLine = workingLine.dropFirst(charactersUsed)
+            print("characters: \(charactersUsed) text: \(text) lexeme: \(lexeme)")
+        }
+    }
+}
+    
+
 func main() {
     do {
-        let lexemes: [Lexeme] = try Lexeme.nextLexeme(from: " var x = 123 var ")
-        print(lexemes)
+        let line = " var x = 123 var "
+        try LexemeSource.lex(line: line)
     } catch {
         print("Failed because \(error).")
     }
