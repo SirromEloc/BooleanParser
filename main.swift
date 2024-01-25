377cc7b4a401e23c7e22828f7fb3a481a3897c3c
SirromEloc <aidan.cole.morris@gmail.com>
2024-01-25 09:07:43 -0600

    shift from demonstration mathematical evaluator to true boolean evaluator

--- main.swift
+++ main.swift
@@ -3,29 +3,26 @@ import Foundation
 enum Lexeme {
     enum LexemeError: Error {
         case failedToFindLexeme(inLine: String)
-        case inconsistentSpacingForOperator(operator: String, inLine: String)
         
     }
 
-    case variable(name: String)
+    case chunk(name: String)
     case operators(value: String)
 
     static func nextLexeme(from line: Substring) throws ->
-      (lexeme: Lexeme, line: String, charactersUsed: Int) {
-        if let match = try /\s*([a-z]+)\s/.firstMatch(in: line) {
-            return (lexeme: Lexeme.variable(name: String(match.1)),
-                    line: String(match.0),
-                    charactersUsed: match.0.count)
-                        
-        } else if let match = try /(\s*)(\+)(\s*)/.prefixMatch(in: line) { //only suited for plus sign currently
-            guard (match.1.count == 0 && match.3.count == 0) ||
-                    (match.1.count > 0 && match.3.count > 0) else {
-                throw LexemeError.inconsistentSpacingForOperator(operator: "+", inLine: String(line))
-            }
+      (lexeme: Lexeme, line: Substring, charactersUsed: Int) {
+        if let match = try /\s*([a-z]+|[\+]+)\s/.firstMatch(in: line) { //finds first lexeme
             
-            return (lexeme: Lexeme.operators(value: String(match.2)),
-                    line: String(match.0),
+            if match.1.contains(/(\+)/) { //checks if lexeme is an operator and returns
+                return (lexeme: Lexeme.operators(value: String(match.1)),
+                        line: match.0,
+                        charactersUsed: match.0.count)
+                
+            } else { //returns non variables
+                return (lexeme: Lexeme.chunk(name: String(match.1)),
+                    line: match.0,
                     charactersUsed: match.0.count)
+            }
             
         } else {
             throw LexemeError.failedToFindLexeme(inLine: String(line))
@@ -36,24 +33,39 @@ enum Lexeme {
 struct LexemeSource {
     let lexeme: Lexeme
     let text: String
-    let lineIndex: Int
-    let columnIndex: Int
+    static var allLexemes: [Lexeme] = []
+    static var onlyVariables: [Lexeme] = []
 
-    static func lex(line: String) throws {
+    static func lex(line: String) throws ->
+      (lexemes: [Lexeme], variables: [Lexeme]) {
         var workingLine = Substring(line)
         while workingLine.count > 0 {
-            let (lexeme, text, charactersUsed) = try Lexeme.nextLexeme(from: workingLine)
+            let (lexeme, text, charactersUsed) = try Lexeme.nextLexeme(from: workingLine) //iterates through lexemes
+
+            allLexemes.append(lexeme)
+            if case .chunk = lexeme {
+                onlyVariables.append(lexeme)
+            }
+            
             workingLine = workingLine.dropFirst(charactersUsed)
-            print("characters: \(charactersUsed) text: \(text) lexeme: \(lexeme)")
+            print("characters: '\(charactersUsed)' text: '\(text)' lexeme: '\(lexeme)'")
         }
+        return (lexemes: allLexemes, variables: onlyVariables)
     }
 }
     
+func parseTree(from origin: [Lexeme]) throws {
+    for lex in origin {
+        if case .operators = lex {
+            
+        }
+}
 
 func main() {
     do {
-        let line = " var x = 123 var "
-        try LexemeSource.lex(line: line)
+        let line = " var x   +  mar "
+        let (lexemes, variables) = try LexemeSource.lex(line: line)
+        print(lexemes, variables)
     } catch {
         print("Failed because \(error).")
     }
